90c7412451919dfdf9ef2f86659d48fee7a608d2
yamadabc <57384471+yamadabc@users.noreply.github.com>
2020-09-16 17:30:06 +0900

    計算機完成

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,48 @@
 <template>
-	<h1>Hello Vue!</h1>
+	<div>
+		<h1>坪単価計算</h1>
+		<div class="container">
+			<h2>売買</h2>
+			<div class="row">
+				<calculator></calculator>
+			</div>
+			<div class="row">
+				<calculator></calculator>
+			</div>
+			<div class="row">
+				<calculator></calculator>
+			</div>
+		</div>
+		<div class='container'>
+			<h2>賃貸</h2>
+			<div class="row">
+				<calculator></calculator>
+			</div>
+			<div class="row">
+				<calculator></calculator>
+			</div>
+			<div class="row">
+				<calculator></calculator>
+			</div>
+		</div>
+	</div>
 </template>
 <script>
+	import Vue from 'vue'
+	import calculator from '~/components/calculator.vue'
 
+	export default Vue.extend({
+		components: {
+			calculator
+		},
+		data: function () {
+			return {
+				data: {
+					price: '',
+					area: '',
+					unitPrice: '',
+				},
+			}
+		},
+	})
 </script>
